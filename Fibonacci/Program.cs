d1a7ba99a1718b25c931344b814f920752fd1f82
Sung <sung.liu@coscomms.com>
2021-03-15 14:15:07 +0800

    Finish Fibonacci sequence project.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fibonacci sequence.");
+
+        InputNumber:
+            Console.Write("Please input the order you want:");
+            string inputValue = Console.ReadLine();
+
+            if (!int.TryParse(inputValue, out int intValue))
+            {
+                goto InputNumber;
+            }
+
+            List<int> sequence = new Fibonacci(intValue).Sequence;
+
+            Console.WriteLine($"\nThe result is {sequence.LastOrDefault()}");
+            Console.WriteLine($"The whole sequence is {JsonConvert.SerializeObject(sequence)}");
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
