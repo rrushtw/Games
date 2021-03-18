1adf601819c3b12327f49e073047544e34557b87
Sung-Ai <rrushtw@gmail.com>
2021-03-18 09:20:32 +0800

    Add ZigZagConversion question.

---
+++ LeeCode/ZigZagConversion/ZigZagClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeeCode.ZigZagConversion
+{
+    public class ZigZagClass : IZigZag
+    {
+        public string Convert(string s, int numRows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
