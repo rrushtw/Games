91e13a9e83643e1aa6b108cc73ad7b744e98f4cd
Sung-Ai <rrushtw@gmail.com>
2021-03-18 16:56:05 +0800

    Complete ZigZag question.

--- LeeCode/ZigZagConversion/ZigZagClass.cs
+++ LeeCode/ZigZagConversion/ZigZagClass.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace LeeCode.ZigZagConversion
 {
@@ -8,7 +6,38 @@ namespace LeeCode.ZigZagConversion
     {
         public string Convert(string s, int numRows)
         {
-            throw new NotImplementedException();
+            if (s.Length <= numRows || numRows == 1)
+            {
+                return s;
+            }
+
+            List<string> table = new List<string>();
+            for (int i = 0; i < numRows; i++)
+            {
+                table.Add(string.Empty);
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int quotient = i % (2 * numRows - 2);
+
+                if (quotient < numRows)
+                {
+                    table[quotient] += s[i];
+                    continue;
+                }
+
+                int index = (2 * numRows - 2) - quotient;
+                table[index] += s[i];
+            }
+
+            string result = string.Empty;
+            foreach (string text in table)
+            {
+                result += text;
+            }
+
+            return result;
         }
     }
 }
