4fa0166c4b5619fca02a33310c30f9a502544c85
Sung-Ai <rrushtw@gmail.com>
2023-04-12 19:53:52 +0800

    Add tests. solution5 failed effeciency test.

---
+++ LeeCode/TSMCTest/Solution4.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeeCode.TSMCTest
+{
+    public interface ISolution4
+    {
+        List<int> minChairs(List<string> simulations);
+    }
+
+    public class Solution4 : ISolution4
+    {
+        public List<int> minChairs(List<string> simulations)
+        {
+            List<int> result = new List<int>();
+
+            foreach (string c in simulations)
+            {
+                int takenChairs = 0;
+                int availibleChairs = 0;
+
+                foreach (char action in c)
+                {
+                    if (action == 'C' || action == 'U')
+                    {
+                        if (availibleChairs == 0)
+                        {
+                            takenChairs++;
+                            continue;
+                        }
+
+                        availibleChairs--;
+                        takenChairs++;
+                        continue;
+                    }
+
+                    if (action == 'R' || action == 'L')
+                    {
+                        takenChairs--;
+                        availibleChairs++;
+                    }
+
+                    continue;
+                }
+
+                result.Add(takenChairs + availibleChairs);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeeCode/TSMCTest/Solution5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeeCode.TSMCTest
+{
+    public interface ISolution5
+    {
+        int TimeOfBuffering(int arrivalRate, List<int> packets);
+    }
+
+    public class Solution5 : ISolution5
+    {
+        public int TimeOfBuffering(int arrivalRate, List<int> packets)
+        {
+            int upperlimit = packets.Max();
+            List<int> buffer = new List<int>();
+
+            for (int i = 0; i < upperlimit; i++)
+            {
+                if (!buffer.Any(x => x == i + 1) && packets.FirstOrDefault() != (i + 1))
+                {
+                    return i + 1;
+                }
+
+                if (packets.Count > 0)
+                {
+                    buffer.AddRange(packets.Take(arrivalRate).Where(x => x > i));
+
+                    if (packets.Count < arrivalRate)
+                    {
+                        packets.Clear();
+                        continue;
+                    }
+
+                    packets.RemoveRange(0, arrivalRate);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
