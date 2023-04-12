481c596bc9283a54085a950adcf50ea1410dcc2c
Sung-Ai <rrushtw@gmail.com>
2023-04-12 14:19:30 +0800

    Add TSMC tests which were for summer intern.

---
+++ LeeCode/TSMCTest/Solution3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeeCode.TSMCTest
+{
+    public class Node
+    {
+        public Node Parent { get; set; }
+
+        public int Position { get; set; }
+        public int Value { get; set; }
+    }
+
+    public interface ISolution3
+    {
+        int bestSumDownwardTreePath(List<int> parent, List<int> values);
+    }
+
+    public class Solution3 : ISolution3
+    {
+        public int bestSumDownwardTreePath(List<int> parent, List<int> values)
+        {
+
+        }
+    }
+}
