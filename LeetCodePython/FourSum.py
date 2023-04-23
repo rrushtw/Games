65fb8f8a8c9e9d342187fb9917b3c2b94ba62a7b
Sung-Ai <rrushtw@gmail.com>
2023-04-23 21:57:32 +0800

    Have a better way to solve 4 sum.

--- LeetCodePython/FourSum.py
+++ LeetCodePython/FourSum.py
@@ -8,371 +8,46 @@ class FourSum:
     # end def
 
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
-        result: list[list[int]] = []
+        nums = sorted(nums)
+        result = []
 
-        positives: list[int] = []
-        zeros: list[int] = []
-        negatives: list[int] = []
+        for i in range(len(nums) - 3):
+            for j in range(i + 1, len(nums) - 2):
+                m = j + 1
+                n = len(nums) - 1
 
-        for number in sorted(nums):
-            if number < 0:
-                negatives.append(number)
-                continue
-
-            if number == 0:
-                zeros.append(number)
-                continue
-
-            if number > 0:
-                positives.append(number)
-                continue
-        # end loop
-
-        # region 4 negative
-        if target < 0 and len(negatives) >= 4:
-            for i in range(len(negatives) - 3):
-                for j in range(i + 1, len(negatives) - 2):
-                    m = j + 1
-                    n = len(negatives) - 1
-
-                    while m < n:
-                        sum = negatives[i] + negatives[j] + \
-                            negatives[m] + negatives[n]
-
-                        if sum == target:
-                            result.append(
-                                [negatives[i], negatives[j], negatives[m], negatives[n]])
-
-                            m += 1
-                            n -= 1
-                            continue
-
-                        if sum < target:
-                            m += 1
-                            continue
-
-                        if sum > target:
-                            n -= 1
-                            continue
-                    # end loop
-                # end loop
-            # end loop
-        # endregion
-
-        # region 3 negative, 1 zero
-        if target < 0 and len(negatives) >= 3 and len(zeros) >= 1:
-            for i in range(len(negatives) - 2):
-                j = i + 1
-                k = len(negatives) - 1
-
-                while j < k:
-                    sum = negatives[i] + negatives[j] + negatives[k]
+                while m < n:
+                    sum = nums[i] + nums[j] + nums[m] + nums[n]
+                    tuple = [nums[i], nums[j], nums[m], nums[n]]
 
                     if sum == target:
-                        result.append(
-                            [negatives[i], negatives[j], negatives[k], 0])
+                        if tuple not in result:
+                            result.append(tuple)
 
-                        j += 1
-                        k -= 1
-                        continue
-
-                    if sum < target:
-                        j += 1
-                        continue
-
-                    if sum > target:
-                        k -= 1
-                        continue
-                # end loop
-            # end loop
-        # endregion
-
-        # region 3 negative, 1 positive
-        if len(negatives) >= 3 and len(positives) >= 1:
-            for p in positives:
-                for i in range(len(negatives) - 2):
-                    j = i + 1
-                    k = len(negatives) - 1
-
-                    while j < k:
-                        sum = negatives[i] + negatives[j] + negatives[k] + p
-
-                        if sum == target:
-                            result.append(
-                                [negatives[i], negatives[j], negatives[k], p])
-
-                            j += 1
-                            k -= 1
-                            continue
-
-                        if sum < target:
-                            j += 1
-                            continue
-
-                        if sum > target:
-                            k -= 1
-                            continue
-                    # end loop
-                # end loop
-            # end loop
-        # endregion
-
-        # region 2 negative, 2 zero
-        if target < 0 and len(negatives) >= 2 and len(zeros) >= 2:
-            i: int = 0
-            j: int = len(negatives) - 1
-
-            while i < j:
-                sum = negatives[i] + negatives[j]
-
-                if sum == target:
-                    result.append([negatives[i], negatives[j], 0, 0])
-                    break
-
-                if sum < target:
-                    i += 1
-                    continue
-
-                if sum > target:
-                    j -= 1
-                    continue
-            # end loop
-        # endregion
-
-        # region 2 negative, 1 zero, 1 postive
-        if len(negatives) >= 2 and len(zeros) >= 1 and len(positives) >= 1:
-            for p in positives:
-                i: int = 0
-                j: int = len(negatives) - 1
-
-                while i < j:
-                    sum = negatives[i] + negatives[j] + p
-
-                    if sum == target:
-                        result.append([negatives[i], negatives[j], 0, p])
-
-                        i += 1
-                        j -= 1
-                        continue
-
-                    if sum < target:
-                        i += 1
-                        continue
-
-                    if sum > target:
-                        j -= 1
-                        continue
-                # end loop
-            # end loop
-        # endregion
-
-        # region 2 negative, 2 postive
-        if len(negatives) >= 2 and len(positives) >= 2:
-            for i in range(len(negatives) - 1):
-                for j in range(i + 1, len(negatives)):
-                    for m in range(len(positives) - 1):
-                        for n in range(m + 1, len(positives)):
-                            sum = negatives[i] + negatives[j] + \
-                                positives[m] + positives[n]
-
-                            if sum == target:
-                                result.append(
-                                    [negatives[i], negatives[j], positives[m], positives[n]])
+                        m += 1
+                        while m < n and nums[m - 1] == nums[m]:
+                            m += 1
                         # end loop
-                    # end loop
-                # end loop
-            # end loop
-        # endregion
 
-        # region 1 negative, 3 zero
-        if target < 0 and len(negatives) >= 1 and len(zeros) >= 3:
-            if any(n == target for n in negatives):
-                result.append([target, 0, 0, 0])
-        # endregion
+                        n -= 1
+                        while m < n and nums[n + 1] == nums[n]:
+                            n -= 1
+                        # end loop
 
-        # region 1 negative, 2 zero, 1 postive
-        if len(negatives) >= 1 and len(zeros) >= 2 and len(positives) >= 1:
-            i = 0
-            j = len(positives) - 1
-
-            while i < len(negatives) and j >= 0:
-                sum = negatives[i] + positives[j]
-
-                if sum == target:
-                    result.append([negatives[i], 0, 0, positives[j]])
-
-                    i += 1
-                    j -= 1
-                    continue
-
-                if sum < target:
-                    i += 1
-                    continue
-
-                if sum > target:
-                    j -= 1
-                    continue
-            # end loop
-        # endregion
-
-        # region 1 negative, 1 zero, 2 postive
-        if len(negatives) >= 1 and len(zeros) >= 1 and len(positives) >= 2:
-            for n in negatives:
-                i = 0
-                j = len(positives) - 1
-
-                while i < j:
-                    sum = n + positives[i] + positives[j]
-
-                    if sum == target:
-                        result.append([n, 0, positives[i], positives[j]])
-
-                        i += 1
-                        j -= 1
                         continue
 
                     if sum < target:
-                        i += 1
+                        m += 1
                         continue
 
                     if sum > target:
-                        j -= 1
+                        n -= 1
                         continue
                 # end loop
             # end loop
-        # endregion
-
-        # region 1 negative, 3 postive
-        if len(negatives) >= 1 and len(positives) >= 3:
-            for n in negatives:
-                for i in range(len(positives) - 2):
-                    j = i + 1
-                    k = len(positives) - 1
-
-                    while j < k:
-                        sum = n + positives[i] + positives[j] + positives[k]
-
-                        if sum == target:
-                            result.append(
-                                [n, positives[i], positives[j], positives[k]])
-                            j += 1
-                            k -= 1
-                            continue
-
-                        if sum < target:
-                            j += 1
-                            continue
-
-                        if sum > target:
-                            k -= 1
-                            continue
-                    # end loop
-                # end loop
-            # end loop
-        # endregion
-
-        # region 4 zero
-        if target == 0 and len(zeros) >= 4:
-            result.append([0, 0, 0, 0])
-        # endregion
-
-        # region 3 zero, 1 postive
-        if target > 0 and len(zeros) >= 3 and len(positives) >= 1:
-            if any(p == target for p in positives):
-                result.append([0, 0, 0, target])
-        # endregion
-
-        # region 2 zero, 2 postive
-        if target > 0 and len(zeros) >= 2 and len(positives) >= 2:
-            i = 0
-            j = len(positives) - 1
-
-            while i < j:
-                sum = positives[i] + positives[j]
-
-                if sum == target:
-                    result.append([0, 0, positives[i], positives[j]])
-
-                    i += 1
-                    j -= 1
-                    continue
-
-                if sum < target:
-                    i += 1
-                    continue
-
-                if sum > target:
-                    j -= 1
-                    continue
-        # endregion
-
-        # region 1 zero, 3 postive
-        if target > 0 and len(zeros) >= 1 and len(positives) >= 3:
-            for i in range(len(positives) - 2):
-                j = i + 1
-                k = len(positives) - 1
-
-                while j < k:
-                    sum = positives[i] + positives[j] + positives[k]
-
-                    if sum == target:
-                        result.append(
-                            [0, positives[i], positives[j], positives[k]])
-
-                        j += 1
-                        k -= 1
-                        continue
-
-                    if sum < target:
-                        j += 1
-                        continue
-
-                    if sum > target:
-                        k -= 1
-                        continue
-                # end loop
-            # end loop
-        # endregion
-
-        # region 4 postive
-        if target > 0 and len(positives) >= 4:
-            for i in range(len(positives) - 3):
-                for j in range(i + 1, len(positives) - 2):
-                    m = j + 1
-                    n = len(positives) - 1
-
-                    while m < n:
-                        sum = positives[i] + positives[j] + \
-                            positives[m] + positives[n]
-
-                        if sum == target:
-                            result.append(
-                                [positives[i], positives[j], positives[m], positives[n]])
-
-                            m += 1
-                            n -= 1
-                            continue
-
-                        if sum < target:
-                            m += 1
-                            continue
-
-                        if sum > target:
-                            n -= 1
-                            continue
-                    # end loop
-                # end loop
-            # end loop
-        # endregion
-
-        uniqueSet = []
-
-        for i in result:
-            if i not in uniqueSet:
-                uniqueSet.append(i)
         # end loop
 
-        return uniqueSet
+        return result
     # end def
 # end class
 
