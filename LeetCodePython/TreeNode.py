e209515fcea28d9958ec45cbbeddea260c6b4112
Sung-Ai <rrushtw@gmail.com>
2023-08-29 00:33:31 +0800

    Warm up.

---
+++ LeetCodePython/TreeNode.py
@@ -0,0 +1,7 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val: int = val
+        self.left: TreeNode = left
+        self.right: TreeNode = right
+    # end def
+# end class
